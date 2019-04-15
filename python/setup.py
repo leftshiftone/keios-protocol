1e584644526c54a53a18827ce3a08f2987f31beb
fridayy <bnjm@harmless.ninja>
2019-04-15 22:40:09 +0200

    setuptools added

---
+++ python/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+# dependencies
+install_requires = []
+extras_require = {}
+
+VERSION = "0.1.0"
+
+setup(name='keios-protocol',
+      version=VERSION,
+      description="keios-protocol",
+      long_description="tbd",
+      author='Leftshift One',
+      author_email='devs@leftshift.one',
+      url='http://www.leftshift.one/',
+      license='beerware',
+      packages=find_packages(exclude=['ez_setup', 'examples',
+                                      'tests', 'tests.*', 'release']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require=extras_require
+)
